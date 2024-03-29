ddc8c62f54db123f041a575bca0289a9c3fddeb3
Hao-Tse Wu <maxwuhz0624@gmail.com>
2024-03-28 22:41:56 -0700

    Fix issue with split_string_with_number_and_double_asterisks function

--- main.py
+++ main.py
@@ -837,7 +837,7 @@ def construct_query_rewrite_prompt(question_list, origin_question, documents):
 def split_string_with_number_and_double_asterisks(input_string):
     # Using regular expression to split the string
     substrings = re.findall(r'\d+\..*?\*\*', input_string)
-    
+    print("substrings: " + substrings )
     # Remove "**" and the number from each substring
     cleaned_substrings = [re.sub(r'\d+\.\s*', '', substring[:-2]) for substring in substrings]
     
